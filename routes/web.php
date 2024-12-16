eb625e9d6fbf19d79855d3a990d2b053cc530b86
emirgokdas <boyteks.bt@boyteks.com>
2024-12-16 16:48:54 +0300

    home change

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
-Route::get('/home', function () {
-    return view('home');
-});
+Route::get('/',[HomeController::class, 'index']);
+
