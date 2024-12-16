b06bdfc5cbd892fc350599fee946facc657eb0aa
emirgokdas <boyteks.bt@boyteks.com>
2024-12-16 16:54:45 +0300

    layout added

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,12 +1,11 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Home</title>
-</head>
-<body>
-    
-</body>
-</html>
+@extends('layout.app')
+@section('title')
+Bayiler Modülü
+@endsection
+@push('css')
+    <link href="{{ URL::asset('build/plugins/datatable/css/dataTables.bootstrap5.min.css') }}" rel="stylesheet" />
+@endpush
+@section('content')
+
+@endsection
+
---
+++ resources/views/layout/app.blade.php
@@ -0,0 +1,40 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" data-bs-theme="blue-theme">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>@yield('title') | emirgokdas</title>
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <link rel="icon" href="{{ URL::asset('build/images/favicon-32x32.png') }}" type="image/png">
+
+    @include('layout.head-css')
+</head>
+
+<body>
+
+    @include('layout.topbar')
+
+    @include('layout.sidebar')
+    
+    <!--start main wrapper-->
+    <main class="main-wrapper">
+        <div class="main-content">
+            @yield('content')
+        </div>
+    </main>
+
+    <!--start overlay-->
+    <div class="overlay btn-toggle"></div>
+    <!--end overlay-->
+
+    {{-- @include('layout.extra')
+
+    @include('layout.common-scripts') --}}
+</body>
+
+</html>
